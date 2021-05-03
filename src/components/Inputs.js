64b54395eebb2b81e1f70f00b1001b83a27a3821
LeBarney <barneywarframe@gmail.com>
2021-05-03 18:04:47 +0200

    add

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,35 +1,66 @@
-const Inputs = ({setValueSlider, valueSlider, text, setText}) => {
+const Inputs = ({
+  setValueSlider,
+  valueSlider,
+  text,
+  setText,
+  setTextSelect,
+}) => {
+  const handleChange = (event) => {
+    setValueSlider(event.target.value);
+  };
 
-const handleChange = (event) => {
-  setValueSlider(event.target.value)
-}
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
 
-const handleTextChange = (event) => {
-  setText(event.target.value)
-}
- 
-return (
-  <div className="col-lg-3 mb-4">
-    <div style={{position: "sticky", top: 0}}>
+  const handleSelectChange = (event) => {
+    setTextSelect(event.target.value);
+  };
 
-        <label htmlFor="sort" className="fw-bold mb-2">Afficher des polices</label>
-        <select id="sort" className="form-select mb-4">
+  return (
+    <div className="col-lg-3 mb-4">
+      <div style={{ position: "sticky", top: 0 }}>
+        <label htmlFor="sort" className="fw-bold mb-2">
+          Afficher des polices
+        </label>
+        <select
+          id="sort"
+          className="form-select mb-4"
+          onChange={handleSelectChange}
+        >
           <option value="date">Les 10 plus récentes</option>
           <option value="popularity">Les 10 plus populaires</option>
           <option value="trending">Top 10 trendings</option>
         </select>
 
         <div className="mb-3">
-          <label htmlFor="text" className="form-label fw-bold mb-3">Tapez votre text</label>
-          <textarea onChange={handleTextChange} id="text" className="form-control">{text}</textarea>
+          <label htmlFor="text" className="form-label fw-bold mb-3">
+            Tapez votre text
+          </label>
+          <textarea
+            onChange={handleTextChange}
+            id="text"
+            className="form-control"
+          >
+            {text}
+          </textarea>
         </div>
 
-        <label htmlFor="range" className="form-label fw-bold mb-3">La taille de police</label>
-        <input onChange={handleChange} type="range" className="form-range" min="8" max="48" step="1" value={valueSlider}></input>
-        
+        <label htmlFor="range" className="form-label fw-bold mb-3">
+          La taille de police
+        </label>
+        <input
+          onChange={handleChange}
+          type="range"
+          className="form-range"
+          min="8"
+          max="48"
+          step="1"
+          value={valueSlider}
+        ></input>
       </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Inputs
+export default Inputs;
