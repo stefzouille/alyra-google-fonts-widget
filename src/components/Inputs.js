7ae43a3d72a316d233dab4350506411be58b4291
johnfr14 <jonathan.alyra@gmail.com>
2021-05-03 16:55:32 +0200

    className

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -10,17 +10,17 @@ const handleChange = (event) => {
 return (
   <div className="col-lg-3 mb-4">
     <div style={{position: "sticky", top: 0}}>
-        <label htmlFor="sort" class="fw-bold mb-2">Afficher des polices</label>
-        <select id="sort" class="form-select mb-4">
+        <label htmlFor="sort" className="fw-bold mb-2">Afficher des polices</label>
+        <select id="sort" className="form-select mb-4">
           <option value="date">Les 10 plus récentes</option>
           <option value="popularity">Les 10 plus populaires</option>
           <option value="trending">Top 10 trendings</option>
         </select>
-        <div class="mb-3">
-          <label htmlFor="text" class="form-label fw-bold mb-3">Tapez votre text</label>
-          <textarea id="text" class="form-control">Portez ce vieux whisky au juge blond qui fume !? 0123456789</textarea>
+        <div className="mb-3">
+          <label htmlFor="text" className="form-label fw-bold mb-3">Tapez votre text</label>
+          <textarea id="text" className="form-control">Portez ce vieux whisky au juge blond qui fume !? 0123456789</textarea>
         </div>
-        <label htmlFor="range" class="form-label fw-bold mb-3">La taille de police</label>
+        <label htmlFor="range" className="form-label fw-bold mb-3">La taille de police</label>
         <input onChange={handleChange} type="range" className="form-range" min="8" max="48" step="1" value={value}></input>
       </div>
   </div>
