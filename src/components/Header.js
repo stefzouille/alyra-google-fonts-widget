110ae2c4e2f48ef53a80d9b8b27f2dc25a190f0f
johnfr14 <jonathan.alyra@gmail.com>
2021-05-03 16:43:43 +0200

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,9 @@
 const Header = () => {
-  return (<></>)
+  return (
+       <header class="p-5 bg-danger text-white text-center">
+        <h1 class="display-1">Quoi de neuf, Google Fonts™&nbsp;?</h1>
+        <p class="h4">Le plus récentes, les plus <i>trendy</i> et les plus populaires polices Google.</p>
+      </header>
+  )
 }
 export default Header
