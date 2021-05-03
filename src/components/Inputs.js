00bac9ecbd021f411c2a797bc051660012f2a764
johnfr14 <jonathan.alyra@gmail.com>
2021-05-03 15:57:59 +0200

    good

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -11,17 +11,11 @@ return (
         </select>
         <div class="mb-3">
           <label htmlFor="text" class="form-label fw-bold mb-3">Tapez votre text</label>
+          <textarea id="text" class="form-control">Portez ce vieux whisky au juge blond qui fume !? 0123456789</textarea>
         </div>
+        <label htmlFor="range" class="form-label fw-bold mb-3">La taille de police</label>
+        <input type="range" class="form-range" min="8" max="48" step="1" value="33"></input>
       </div>
-      <div class="mb-3">
-        
-        <input type="password" class="form-control" id="exampleInputPassword1" />
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
   </div>
   )
 }
