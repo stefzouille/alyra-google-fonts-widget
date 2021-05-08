ecb25baf3d62fe684909b6b888fbb26a351f01b5
johnfr14 <jonathan.alyra@gmail.com>
2021-05-08 22:12:56 +0200

    Time.js le come back, malgré l'abandon par steph

---
+++ src/components/Time.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class App extends React.Component {
+  state = {
+    date: "" , 
+  }
+ componentDidMount() {
+    this.getDate();
+  }
+   getDate = () => {
+    var date = new Date().toLocaleString()
+    this.setState({ date });
+    window.setInterval(() => this.getDate(), 1000)
+  };
+  render() {
+    const { date } = this.state;
+      return <div className="text-center text-danger fs-3">{date}</div>;
+  }
+}
