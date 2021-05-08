82b9ef911244d58fb19636fa3fad226238e80ef1
johnfr14 <jonathan.alyra@gmail.com>
2021-05-08 20:05:25 +0200

    Revert "subsets"
    This reverts commit 4da91b22de24122115784cac0f26152b371c1f57.

--- src/components/Fonts.js
+++ src/components/Fonts.js
@@ -3,6 +3,13 @@
 
 
 const Fonts = ({ valueSlider, text, sort, font }) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const link = `https://fonts.google.com/specimen/${font.family}`
+//tets pull
+  // useEffect(() => {
+  //   setLoading(true);
+  //   setError("");
 
  return (
     
@@ -19,18 +26,30 @@ const Fonts = ({ valueSlider, text, sort, font }) => {
               {text}
             </p>
 
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              className="text-danger"
-              href="https://fonts.google.com/specimen/Karantina"
-            >
-              Voir sur Google Fonts (ouvre un nouveau tab)
-            </a>
-          </div>
-        </article>
-      </div>
-   
+      <article className="col-lg-6 mb-4">
+       
+        <div className="shadow-sm border p-3 h-100">
+          <h2 className="h6 d-flex aling-items-center justify-content-between">
+            <span>{font.family}</span>
+            <small>{font.variants.length} variant(s)</small>
+          </h2>
+          <span className="badge bg-dark mb-4">{font.category}</span>
+          <GoogleFontLoader fonts={[{font:`${font.family}`, weights: [300,700]}]} subsets={['cyrillic-ext', 'greek']}
+          />
+          <p className="sample" style={{fontFamily: `${font.family}`, fontSize: `${valueSlider}px`}}>
+            {text}
+          </p>
+
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            className="text-danger"
+            href={link}
+          >
+            Voir sur Google Fonts (ouvre un nouveau tab)
+          </a>
+        </div>
+      </article>
   );
 };
 
