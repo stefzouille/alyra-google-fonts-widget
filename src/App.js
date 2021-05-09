fe7b39be05ae8297d84501386a10538f736b3f11
stefzouille <contact.lokcy@gmail.com>
2021-05-09 21:02:25 +0200

    good rajout du dark mode bro's

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
+
+import React from "react"
 import Page from './components/Page'
 import Header from './components/Header'
 import DateTime from './components/Time'
 
 
-
+const ToggleModeButton = (props) => {
+  const { darkMode, setDarkMode } = props;
+  const handleButtonClick = () => {
+    setDarkMode(!darkMode);
+  };
+  return (
+    <button onClick={handleButtonClick} className="btn btn-danger mb-4">
+      Dark Mode
+    </button>
+  );
+};
 
 
 function App() {
+ const [darkMode, setDarkMode] = React.useState(true);
+  const modeClasses = darkMode ? "bg-dark text-white" : "bg-light";
+
   return (
-    <div className="App">
+    <section className={modeClasses}>
+    <div className="App container">
        
       <Header />
       <DateTime />
+      <ToggleModeButton darkMode={darkMode} setDarkMode={setDarkMode} />
       <Page />
       <footer className="p-3 text-center bg-dark text-white">With Love &amp; React for Alyra</footer>
     </div>
+    </section>
   );
 }
 export default App;
