d0d9e6a341e4164013de2fb4c2f34439a3289e94
stefzouille <contact.lokcy@gmail.com>
2021-05-10 15:12:57 +0200

    rm

--- src/components/Page.js
+++ src/components/Page.js
@@ -29,11 +29,7 @@ const Page = () => {
     setError("");
 
     fetch(`https://www.googleapis.com/webfonts/v1/webfonts?sort=${sort}&key=${process.env.REACT_APP_SECRET_NAME}`)
-      .then((response) => {
-        return new Promise((resolved) => {
-          // setTimeout(() => resolved(response), 200);
-        });
-      })
+      
       .then((response) => {
         if (!response.ok) {
           throw new error(`impossible de lire les Fonts ${response.status}`)
